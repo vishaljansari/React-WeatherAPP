1ba79efb2e9fa6b5dddca9a055119fb4068b0f9f
VISHAL JANSARI <vishaljansari@hotmail.com>
2018-03-08 00:08:31 -0800

    done

---
+++ src/components/Titles.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Titles = () => (
+
+    <div>
+        <h1 className="title-container__title"> WEATHER </h1>
+        <h3 className="title-container__subtitle">Findout temperature, conditions and more....</h3>
+    </div>
+
+);
+
+export default Titles;
